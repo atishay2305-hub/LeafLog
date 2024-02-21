65f7cd49a5f8597edac584678ac1b841647ef5f7
Mikayla M <mikayla.mount22@gmail.com>
2024-02-21 18:19:04 -0500

    stylized header, added to sign up and login page

--- leaf-log/src/app/components/Header.tsx
+++ leaf-log/src/app/components/Header.tsx
@@ -6,16 +6,22 @@ const Header = () => {
     <header className={styles.header}>
       <nav>
         <Link href="/" legacyBehavior>
-          <a id="homeLink">Home</a>
+          <a id="homeLink">Home &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
         </Link>
         <Link href="/my-plants" legacyBehavior>
-          <a id="myPlantsLink">My Plants</a>
+          <a id="myPlantsLink">
+            My Plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+          </a>
         </Link>
         <Link href="/signup" legacyBehavior>
-          <a id="signupLink">Sign Up</a>
+          <a id="signupLink">
+            Sign Up &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </a>
         </Link>
         <Link href="/login" legacyBehavior>
-          <a id="loginLink">Log In</a>
+          <a id="loginLink">
+            Log In &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </a>
         </Link>
         {/* Add other navigation links here */}
       </nav>
--- leaf-log/src/app/signup/page.tsx
+++ leaf-log/src/app/signup/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Head from "next/head";
+import Header from "../components/Header";
 import { useState, FormEvent, ChangeEvent } from "react";
 import styles from "./Signup.module.css";
 
@@ -68,63 +69,66 @@ export default function SignupPage() {
   };
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Sign Up</title>
-        <br />
-        <meta name="description" content="Sign up for our service" />
-      </Head>
-      <div className={styles.formWrapper}>
-        <h1 className={styles.title}>Sign Up</h1>
-        <p className={styles.description}>Please sign up to continue.</p>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <Head>
+          <title>Sign Up</title>
+          <br />
+          <meta name="description" content="Sign up for our service" />
+        </Head>
+        <div className={styles.formWrapper}>
+          <h1 className={styles.title}>Sign Up</h1>
+          <p className={styles.description}>Please sign up to continue.</p>
 
-        {/* Sign up form */}
-        <form onSubmit={handleSubmit} id="signupForm" className={styles.form}>
-          <input
-            className={styles.inputField}
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Email"
-            required
-          />
+          {/* Sign up form */}
+          <form onSubmit={handleSubmit} id="signupForm" className={styles.form}>
+            <input
+              className={styles.inputField}
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+              required
+            />
 
-          <input
-            className={styles.inputField}
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder="Password"
-            required
-          />
+            <input
+              className={styles.inputField}
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Password"
+              required
+            />
+            <button
+              type="submit"
+              id="buttonStyle"
+              className={styles.redirectButton}
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>{" "}
+        <div className={styles.bottom}>
+          {/* Google OAuth sign up */}
           <button
-            type="submit"
-            id="buttonStyle"
-            className={styles.redirectButton}
+            onClick={handleGoogleSignUp}
+            className={styles.googleSignupButton}
           >
-            Sign Up
+            Sign Up with Google
           </button>
-        </form>
-      </div>{" "}
-      <div className={styles.bottom}>
-        {/* Google OAuth sign up */}
-        <button
-          onClick={handleGoogleSignUp}
-          className={styles.googleSignupButton}
-        >
-          Sign Up with Google
-        </button>
-        <br />
-        <br />
+          <br />
+          <br />
 
-        <a className={styles.bottomText}>Have an account?</a>
+          <a className={styles.bottomText}>Have an account?</a>
 
-        <button type="submit" className={styles.redirectButton}>
-          <a href="http://localhost:3000/login">Login</a>
-        </button>
+          <button type="submit" className={styles.redirectButton}>
+            <a href="http://localhost:3000/login">Login</a>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
