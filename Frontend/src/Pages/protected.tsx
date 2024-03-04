674e826f2b8adb3bda315e8084d3b67abff698fb
atishay2305-hub <atishay2305@gmail.com>
2024-03-04 13:30:33 -0500

    Added few dependencies and made changes to user routes

---
+++ Frontend/src/Pages/protected.tsx
@@ -0,0 +1,13 @@
+// Frontend/src/Pages/protected.js
+import React from 'react';
+
+const Protected = ({ displayName }) => {
+  return (
+    <div>
+      <h1>Hello {displayName}</h1>
+      {/* Add your React component logic here */}
+    </div>
+  );
+};
+
+export default Protected;
