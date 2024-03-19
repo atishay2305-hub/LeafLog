4826a44a0274a9ef1f2f3bb6064f2d865febd173
cmclaug <cr.mcl@icloud.com>
2024-03-19 18:49:11 -0400

    Stylized TOS, added and Stylized Start Page

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -3,34 +3,52 @@ import styles from "./Header.module.css"; // Adjust the import path as necessary
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <nav>
-        <Link href="/" legacyBehavior>
-          <a id="homeLink">Home &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-        </Link>
-        <Link href="/plant-log" legacyBehavior>
-          <a id="myPlantsLink">
-            My Plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
-          </a>
-        </Link>
-        <Link href="/signup" legacyBehavior>
-          <a id="signupLink">
-            Sign Up &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </a>
-        </Link>
-        <Link href="/login" legacyBehavior>
-          <a id="loginLink">
-            Log In &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </a>
-        </Link>
-        <Link href="/tos" legacyBehavior>
-          <a id="tosLink">
-            Terms of Service &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </a>
-        </Link>
-        {/* Add other navigation links here */}
-      </nav>
-    </header>
+   <div>
+      <header className={styles.header}>
+        <nav>
+          <div className={styles.LeafLog}>LeafLog</div>
+          <br />
+          <div className={styles.navContainer}>
+            <Link href="/" legacyBehavior>
+              <a id="homeLink" className={styles.navLink}>
+                Home &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </a>
+            </Link>
+            <Link href="/getting-started" legacyBehavior>
+              <a id="startLink" className={styles.navLink}>
+                Getting Started &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+              </a>
+            </Link>
+            <Link href="/plant-log" legacyBehavior>
+              <a id="myPlantsLink" className={styles.navLink}>
+                My Plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+              </a>
+            </Link>
+            <Link href="/search" legacyBehavior>
+              <a id="searchLink" className={styles.navLink}>
+                Discover Plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+              </a>
+            </Link>
+            <Link href="/signup" legacyBehavior>
+              <a id="signupLink" className={styles.navLink}>
+                Sign Up &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </a>
+            </Link>
+            <Link href="/login" legacyBehavior>
+              <a id="loginLink" className={styles.navLink}>
+                Log In &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </a>
+            </Link>
+             <Link href="/tos" legacyBehavior>
+              <a id="loginLink" className={styles.navLink}>
+                Terms of Service &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </a>
+            </Link>
+            {/* Add other navigation links here */}
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 };
 
