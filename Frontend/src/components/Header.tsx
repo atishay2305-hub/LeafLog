ad2862393b3842ad68bf348d2baab694fcb357da
atishay2305-hub <atishay2305@gmail.com>
2024-04-07 18:19:26 -0400

    fixed styling of navbar

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,102 +1,69 @@
-// import Link from "next/link";
-// import React from "react";
-// import "./Header.css"
-
-// const Header = () => {
-//   return (
-//     <div>
-//       <header className="navbar">
-//         <div className="links">
-//           <Link href="/LandingPage" legacyBehavior>
-//             <a id="homeLink" className="navLink">
-//               Home &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-//             </a>
-//           </Link>
-//           <Link href="/plant-log" legacyBehavior>
-//             <a id="myPlantsLink" className="navLink">
-//               My Plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-//             </a>
-//           </Link>
-//           <Link href="/search" legacyBehavior>
-//             <a id="searchLink" className="navLink">
-//               Discover Plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-//             </a>
-//           </Link>
-
-//           <Link href="/profile" legacyBehavior>
-//             <a id="profileLink" className="navLink">
-//               Profile &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-//             </a>
-//           </Link>
-//           <Link href="/" legacyBehavior>
-//             <a id="logoutLink" className="navLink">
-//               Log Out &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-//             </a>
-//           </Link>
-//           <Link href="/tos" legacyBehavior>
-//             <a id="tosLink" className="navLink">
-//               Terms of Service &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-//             </a>
-//           </Link>
-//         </div>
-//       </header>
-//     </div>
-//   );
-// };
-
-// export default Header;
-import Link from "next/link";
 import React from "react";
-import "./Header.css";
 import Cookies from 'js-cookie';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+
   // Function to handle logout
   const handleLogout = () => {
     // Remove the token cookie
     Cookies.remove('token');
     // Redirect to the login page
-    Router.push('/login');
+    router.push('/login');
   };
 
   return (
-    <div>
-      <header className="navbar">
-        <div className="links">
-          <Link href="/LandingPage" legacyBehavior>
-            <a id="homeLink" className="navLink">
-              Home &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </a>
-          </Link>
-          <Link href="/plant-log" legacyBehavior>
-            <a id="myPlantsLink" className="navLink">
-              My Plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </a>
-          </Link>
-          <Link href="/search" legacyBehavior>
-            <a id="searchLink" className="navLink">
-              Discover Plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </a>
-          </Link>
+    <header className="bg-green-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/LandingPage" className="text-white text-2xl font-bold">LeafLog</a>
+        <nav className="hidden md:block">
+          <ul className="flex space-x-4">
+            <NavItem href="/LandingPage">Home</NavItem>
+            <NavItem href="/plant-log">My Plants</NavItem>
+            <NavItem href="/search">Discover Plants</NavItem>
+            <NavItem href="/profile">Profile</NavItem>
+            <NavItem href="/tos">Terms of Service</NavItem>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-gray-200 transition duration-300"
+                onClick={handleLogout}
+              >
+                Log Out
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <button className="md:hidden text-white">
+          {/* Mobile menu button */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+};
 
-          <Link href="/profile" legacyBehavior>
-            <a id="profileLink" className="navLink">
-              Profile &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </a>
-          </Link>
-          {/* Logout link triggers handleLogout function */}
-          <a id="logoutLink" className="navLink" onClick={handleLogout}>
-            Log Out &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </a>
-          <Link href="/tos" legacyBehavior>
-            <a id="tosLink" className="navLink">
-              Terms of Service &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </a>
-          </Link>
-        </div>
-      </header>
-    </div>
+const NavItem = ({ href, children }) => {
+  return (
+    <li>
+      <a href={href} className="text-white hover:text-gray-200 transition duration-300">
+        {children}
+      </a>
+    </li>
   );
 };
 
