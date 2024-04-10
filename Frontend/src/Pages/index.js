361cd60b0bcc0eccc0b4bf104b9f3cd4ee3c4f09
Mikayla M <mikayla.mount22@gmail.com>
2024-04-09 23:12:59 -0400

    landing page styling

--- Frontend/src/Pages/index.js
+++ Frontend/src/Pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
 import DBConnection from "../../../Backend/utils/DBConnection.js";
+import "../styles/global.css";
 
 export default function Home() {
-  DBConnection();
   return (
     <>
       <Head>
@@ -12,17 +12,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="bg-green-200 min-h-screen flex items-center justify-center">
+      <section className="bg-green-300 min-h-screen flex items-center justify-center top-level">
         <div className="max-w-lg p-8 bg-white shadow-lg rounded-lg text-center">
-          <h1 className="text-3xl font-bold text-green-600 mb-4">LeafLog</h1>
-          <div className="space-x-4">
+          <h1 className="text-5xl font-bold text-green-600 mb-6">LeafLog</h1>
+          <div className="space-x-6">
             <Link href="/login">
-              <button className="bg-green-600 hover:bg-green-700 text-white py-2 px-6 rounded-lg font-medium transition duration-300">
+              <button className="bg-green-600 hover:bg-green-700 text-white py-2 px-12 rounded-lg font-medium transition duration-300">
                 Login
               </button>
             </Link>
             <Link href="/register">
-              <button className="bg-green-600 hover:bg-green-700 text-white py-2 px-6 rounded-lg font-medium transition duration-300">
+              <button className="bg-green-600 hover:bg-green-700 text-white py-2 px-12 rounded-lg font-medium transition duration-300">
                 Register
               </button>
             </Link>
--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -13,8 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "custom-green": "#497364",
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
