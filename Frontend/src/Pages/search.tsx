2bdb4ec5629ed8255bdff38328ad1d3384f78552
Mikayla M <mikayla.mount22@gmail.com>
2024-04-10 00:44:21 -0400

    search styling

--- Frontend/src/Pages/search.tsx
+++ Frontend/src/Pages/search.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState, FormEvent, ChangeEvent } from 'react';
-import axios from 'axios';
-import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import styles from './search.module.css';
-import Cookies from 'js-cookie';
-import Router from 'next/router';
+import { useEffect, useState, FormEvent, ChangeEvent } from "react";
+import axios from "axios";
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import styles from "./search.module.css";
+import Cookies from "js-cookie";
+import Router from "next/router";
+import Link from "next/link";
+import "../styles/global.css";
 
 interface Plant {
   _id: string;
@@ -22,22 +24,24 @@ export default function Search() {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const token = Cookies.get('token');
+    const token = Cookies.get("token");
     if (!token) {
-      Router.push('/login');
+      Router.push("/login");
     } else {
       const fetchAllPlants = async () => {
         setLoading(true);
         try {
-          const response = await axios.get('http://localhost:5002/api/plantdata');
+          const response = await axios.get(
+            "http://localhost:5002/api/plantdata"
+          );
           setAllPlants(response.data);
         } catch (error) {
-          setError('An error occurred while fetching plant data.');
+          setError("An error occurred while fetching plant data.");
         } finally {
           setLoading(false);
         }
       };
-  
+
       fetchAllPlants();
     }
   }, []);
@@ -53,7 +57,9 @@ export default function Search() {
 
     try {
       const response = await axios.get(
-        `http://localhost:5002/api/plantdata/search/common_name?name=${encodeURIComponent(plantQuery)}`
+        `http://localhost:5002/api/plantdata/search/common_name?name=${encodeURIComponent(
+          plantQuery
+        )}`
       );
 
       const results = response.data;
@@ -72,8 +78,12 @@ export default function Search() {
   const handleAddToLog = (plant: Plant) => {
     // Navigate to the PlantLog page with plant data
     Router.push({
-      pathname: '/plant-log',
-      query: { plantId: plant._id, commonName: plant.common_name, scientificName: plant.scientific_name }
+      pathname: "/plant-log",
+      query: {
+        plantId: plant._id,
+        commonName: plant.common_name,
+        scientificName: plant.scientific_name,
+      },
     });
   };
 
@@ -83,48 +93,55 @@ export default function Search() {
         <title>Search Plants</title>
       </Head>
       <Header />
-      <div className={styles.container}>
-        <h1 className={styles.title}>Discover Plants</h1>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <input
-            type="text"
-            value={plantQuery}
-            onChange={handleSearchChange}
-            className={styles.searchInput}
-            placeholder="Type and search for a plant species"
-            required
-          />
-          <button type="submit" className={styles.button}>
-            Search
-          </button>
-        </form>
-        <div className={styles.resultsContainer}>
-          <h2>Search Results</h2>
-          {loading && <p>Loading...</p>}
-          {error && <p>{error}</p>}
-          {Array.isArray(searchResults) && searchResults.map((plant, index) => (
-            <div key={index} className={styles.resultItem}>
-              <h3>{plant.common_name}</h3>
-              <p>Scientific Name: {plant.scientific_name}</p>
-              {plant.other_name && <p>Other Name: {plant.other_name}</p>}
-              <button onClick={() => handleAddToLog(plant)}>Add to Log</button>
+      <section className="top-level bg-green-300 min-h-screen flex flex-col items-center justify-center">
+        <div className="bg-white rounded-lg shadow-lg max-w-4xl w-full p-12 text-center mb-8">
+          <h1 className="text-5xl font-bold text-green-600 mb-8">
+            Discover Plants
+          </h1>
+          <form onSubmit={handleSubmit} className="space-y-8">
+            <input
+              type="text"
+              value={plantQuery}
+              onChange={handleSearchChange}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-3"
+              placeholder="Type and search for a plant species"
+              required
+            />
+            <button
+              type="submit"
+              className="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Search
+            </button>
+          </form>
+        </div>{" "}
+        <section className="container mx-auto px-2 py-2 submittedBox max-w-4xl w-full">
+          <div className="space-y-1">
+            <h2 className="text-2xl font-semibold text-center">
+              Search Results
+            </h2>
+            {loading && <p className="text-center">Loading...</p>}
+            {error && <p className="text-center text-red-500">{error}</p>}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {Array.isArray(searchResults) &&
+                searchResults.map((plant, index) => (
+                  <div key={index} className="bg-gray-50 p-6 rounded-lg shadow">
+                    <h3 className="text-lg font-bold">{plant.common_name}</h3>
+                    <p>Scientific Name: {plant.scientific_name}</p>
+                    {plant.other_name && <p>Other Name: {plant.other_name}</p>}
+                    <button
+                      onClick={() => handleAddToLog(plant)}
+                      className="mt-4 text-green-600 hover:underline"
+                    >
+                      Add to Log
+                    </button>
+                  </div>
+                ))}
             </div>
-          ))}
-        </div>
-        <div className={styles.resultsContainer}>
-          <h2>All Plants</h2>
-          {loading && <p>Loading...</p>}
-          {error && <p>{error}</p>}
-          {allPlants.map((plant, index) => (
-            <div key={index} className={styles.resultItem}>
-              <h3>{plant.common_name}</h3>
-              <p>Scientific Name: {plant.scientific_name}</p>
-              {plant.other_name && <p>Other Name: {plant.other_name}</p>}
-              <button onClick={() => handleAddToLog(plant)}>Add to Log</button>
-            </div>
-          ))}
-        </div>
-      </div>
+          </div>
+        </section>
+      </section>
+
       <Footer />
     </>
   );
