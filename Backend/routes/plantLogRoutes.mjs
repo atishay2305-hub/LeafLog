841031f8a248a53fe685b1cf8eafcaf1698e02a2
atishay2305-hub <atishay2305@gmail.com>
2024-04-17 17:41:30 -0400

    trial: email issue fix

--- Backend/routes/plantLogRoutes.mjs
+++ Backend/routes/plantLogRoutes.mjs
@@ -12,8 +12,8 @@ const router = express.Router();
 router
     .route("/")
     .post(authMiddleware, createPlantLog)
-    .get(authMiddleware, getPlantLogs);
-post("/send-watering-reminder", sendWateringReminder);
+    .get(authMiddleware, getPlantLogs)
+    .post("/send-watering-reminder", sendWateringReminder); // Define the route using router.post
 
 
 export default router;
