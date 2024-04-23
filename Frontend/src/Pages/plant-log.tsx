a71d981574c74711ca78dce14293c25dc5e7cc00
Mikayla M <mikayla.mount22@gmail.com>
2024-04-23 08:02:06 -0400

    fixed search display bug

--- Frontend/src/Pages/plant-log.tsx
+++ Frontend/src/Pages/plant-log.tsx
@@ -182,12 +182,10 @@ const PlantLog = () => {
                         {searchResults.map((plant, index) => (
                           <div
                             key={index} // Changed key to index, as the array index is unique
-                            onClick={() =>
-                              handleSelectPlant(plant.plantSpecies)
-                            }
+                            onClick={() => handleSelectPlant(plant.common_name)}
                             className="dropdown-item"
                           >
-                            {plant.plantSpecies} ({plant.scientificName})
+                            {plant.common_name} ({plant.scientific_name})
                           </div>
                         ))}
                       </div>
@@ -237,6 +235,7 @@ const PlantLog = () => {
                   <option value="daily">Daily</option>
                   <option value="weekly">Weekly</option>
                   <option value="biweekly">Biweekly</option>
+                  <option value="monthly">Monthly</option>
                 </select>
               </div>
 
