dbc86ce24949126e5b971c53ffa754ca3a0f025e
Mikayla M <mikayla.mount22@gmail.com>
2024-04-23 13:15:54 -0400

    profile styling

--- Frontend/src/Pages/profile.js
+++ Frontend/src/Pages/profile.js
@@ -89,7 +89,7 @@ const Profile = () => {
       </Head>
       <Header />
       <div className="top-level bg-green-200 min-h-screen flex justify-center items-center">
-        <div className="bg-white p-8 rounded-lg shadow-md max-w-md">
+        <div className="bg-white p-8 rounded-lg shadow-md max-w-xl w-full">
           <div className="text-center">
             <label htmlFor="profile-picture">
               <img
@@ -108,30 +108,32 @@ const Profile = () => {
             <h2 className="text-xl font-bold mb-2">{user.name}</h2>
             <p className="text-gray-600">{user.email}</p>
           </div>
-          <div className="mt-6">
-            <h3 className="text-lg font-semibold mb-2">Contact Information</h3>
-            <ul>
-              <li className="flex items-center mb-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 mr-2 text-gray-700"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M2 4a2 2 0 012-2h12a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V4zm2-2a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V3a1 1 0 00-1-1H4z"
-                    clipRule="evenodd"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    d="M10 3a1 1 0 100-2 1 1 0 000 2zM5 6a1 1 0 100-2 1 1 0 000 2zM5 8a1 1 0 100-2 1 1 0 000 2zM5 10a1 1 0 100-2 1 1 0 000 2zM5 12a1 1 0 100-2 1 1 0 000 2zM5 14a1 1 0 100-2 1 1 0 000 2zM5 16a1 1 0 100-2 1 1 0 000 2zM15 10a1 1 0 11-2 0 1 1 0 012 0zM15 12a1 1 0 11-2 0 1 1 0 012 0zM15 14a1 1 0 11-2 0 1 1 0 012 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                {user.email}
-              </li>
-            </ul>
+
+          <div className="mt-6 flex flex-col space-y-4">
+            <button
+              onClick={() => Router.push("/my-plants")}
+              className="bg-green-600 hover:bg-green-800 text-white font-semibold py-4 px-4 rounded-lg"
+            >
+              My Plants
+            </button>
+            <button
+              onClick={() => Router.push("/notifications")}
+              className="bg-green-600 hover:bg-green-800 text-white font-semibold py-4 px-4 rounded-lg"
+            >
+              Notifications
+            </button>
+            <button
+              onClick={() => Router.push("/FeedbackForm")}
+              className="bg-green-600 hover:bg-green-800 text-white font-semibold py-4 px-4 rounded-lg"
+            >
+              Send Feedback
+            </button>
+            <button
+              onClick={() => Router.push("/tos")}
+              className="bg-green-600 hover:bg-green-800 text-white font-semibold py-4 px-4 rounded-lg"
+            >
+              Terms of Service
+            </button>
           </div>
         </div>
       </div>
