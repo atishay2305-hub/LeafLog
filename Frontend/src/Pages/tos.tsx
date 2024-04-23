8d2f8fb1fd1e85b93f544bb559aeda361f1e8b8c
atishay2305-hub <atishay2305@gmail.com>
2024-04-23 14:16:58 -0400

    build issue fix

--- Frontend/src/Pages/tos.tsx
+++ Frontend/src/Pages/tos.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import styles from "./tos.css";
 import Cookies from "js-cookie";
 import Router from "next/router";
 
