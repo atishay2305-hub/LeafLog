5e8cfaa98d4b303518392ef1d8e1dd9d1a574468
atishay2305-hub <atishay2305@gmail.com>
2024-04-24 13:32:10 -0400

    Added mongo Atlas configurations

--- Backend/utils/DBConnection.js
+++ Backend/utils/DBConnection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const DBconnection = async () => {
-    const connectionUrl = 'mongodb://localhost:27017/leaflog'; 
+    const connectionUrl = 'mongodb+srv://atishay2305:d1N73QqeRIPbIlAS@leaflog.qc1rin7.mongodb.net/?retryWrites=true&w=majority&appName=leaflog'; 
     mongoose.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log(`Database connected successfully`))
         .catch((err) => console.log("Getting Error from DB connection" + err.message))
