2098ae491fe6b8be6ff673439c123a35d02db0ec
atishay2305-hub <atishay2305@gmail.com>
2024-04-23 23:32:59 -0400

    fixed selenium script bugs

--- Test_Selenium/test.py
+++ Test_Selenium/test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome()
@@ -46,56 +47,145 @@ LOGIN_PASSWORD.send_keys("password123")
 SIGN_IN = driver.find_element(By.XPATH, '//*[@id="__next"]/section/div/form/button')
 SIGN_IN.click()
 
-time.sleep(4)
+
+
+
+# CLICK PROFILE PAGE from navbar
+
+time.sleep(5)
+
+driver.get("http://localhost:3000/profile")
+driver.maximize_window()
+
+time.sleep(2)
 
 driver.get("http://localhost:3000/LandingPage")
 driver.maximize_window()
 
-# CLICK PROFILE PAGE from navbar
-PROFILE_NAVBAR = driver.find_element(By.XPATH, '//*[@id="__next"]/header/div/nav/ul/li[4]/a')
-PROFILE_NAVBAR.click()
-
-time.sleep(2)
-
-# Navigate back
-driver.back()
-
-time.sleep(3)
+time.sleep(5)
 
 # Scroll to the bottom of the page
 scroll_bottom_script = "arguments[0].scrollIntoView({behavior: 'smooth', block: 'end', inline: 'nearest'});"
 driver.execute_script(scroll_bottom_script, driver.find_element(By.TAG_NAME, "footer"))
-time.sleep(2)  # Wait for a few seconds
 
-# Scroll back to the top of the page
-scroll_top_script = "arguments[0].scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});"
-driver.execute_script(scroll_top_script, driver.find_element(By.XPATH, '//*[@id="__next"]'))
-time.sleep(2)  # Wait for a few seconds
 
+time.sleep(5)
 # search a plant
-time.sleep(2)
-driver.get("http://localhost:3000/search")
-driver.maximize_window()
+# time.sleep(5)
+# driver.get("http://localhost:3000/search")
+# driver.maximize_window()
 
-SEARCH = driver.find_element(By.XPATH, '//*[@id="__next"]/div/section/div[1]/form/input')
-SEARCH.click()
-SEARCH.send_keys("e")
+# time.sleep(5)
+# SEARCH = driver.find_element(By.XPATH, '//*[@id="__next"]/div/section/div[1]/form/input')
+# SEARCH.click()
+# SEARCH.send_keys("e")
 
-search_button = driver.find_element(By.XPATH, '//*[@id="__next"]/div/section/div[1]/form/button')
-search_button.click()
+# search_button = driver.find_element(By.XPATH, '//*[@id="__next"]/div/section/div[1]/form/button')
+# search_button.click()
 
-# Click the first "Add to My Plants" button
-first_add_to_my_plants_button = driver.find_element(By.XPATH, '//*[@id="__next"]/div/section/div[2]/div/div[1]/button')
-first_add_to_my_plants_button.click()
-
-
+# # Click the first "Add to My Plants" button
+# first_add_to_my_plants_button = driver.find_element(By.XPATH, '//*[@id="__next"]/div/section/div[2]/div/div[1]/button')
+# first_add_to_my_plants_button.click()
 
 # log a plant manually
+driver.get("http://localhost:3000/plant-log")
+driver.maximize_window()
 
-# see my-plants page
+time.sleep(6)
 
-# logout 
+plant_species = driver.find_element(By.XPATH, '//*[@id="plantSpecies"]')
+plant_species.click()
+plant_species.send_keys("e")
+time.sleep(7)
+specie_name = driver.find_element(By.XPATH, "//div[@id='plantSpeciesContainer']//div[1]")
+specie_name.click()
 
-# TODO: LOGOUT SESSION
+time.sleep(5)
+
+# Find the dropdown element
+dropdown_start = driver.find_element(By.XPATH, '//*[@id="cycle"]')
+
+# Click the dropdown to open it
+dropdown_start.click()
+
+# Wait for the dropdown options to load (adjust the sleep time as needed)
+time.sleep(6)
+
+# Wrap the dropdown element in a Select object
+select = Select(dropdown_start)
+
+# Select the option by visible text
+select.select_by_visible_text("Annual")
+
+main_background = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div')
+main_background.click()
+
+time.sleep(5)
+
+
+dropdown_2 = driver.find_element(By.XPATH, '//*[@id="watering"]')
+dropdown_2.click()
+
+time.sleep(5)
+
+select2 = Select(dropdown_2)
+
+select2.select_by_visible_text("Daily")
+
+main_background = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div')
+main_background.click()
+
+dropdown_3 = driver.find_element(By.XPATH, '//*[@id="sunlight"]')
+dropdown_3.click()
+
+time.sleep(4)
+
+
+select3 = Select(dropdown_3)
+
+select3.select_by_visible_text('Full Sun')
+
+main_background = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div')
+main_background.click()
+
+time.sleep(5)
+
+log_plant_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/form/button')
+log_plant_button.click()
+
+
+time.sleep(3)
+
+
+alert = driver.switch_to.alert
+alert.dismiss()
+
+time.sleep(4)
+
+driver.get("http://localhost:3000/my-plants")
+driver.maximize_screen()
+
+time.sleep(4)
+
+# send notification
+
+driver.get("http://localhost:3000/notifications")
+
+field_element = driver.find_element(By.XPATH, '//*[@id="email"]')
+field_element.click()
+field_element.send_keys("atishay23@gmail.com")
+
+checkbox = driver.find_element(By.XPATH, '//*[@id="66285bd73ff863ad2df39aab"]')
+checkbox.click()
+
+submit_button = driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/div/form/button')
+submit_button.click()
+
+time.sleep(7)
+
+# logout
+logout_button = driver.find_element(By.XPATH, '//*[@id="__next"]/header/div/nav/ul/li[7]/a')
+logout_button.click()
+
 
 driver.close()
