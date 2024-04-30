c3d9efa47a9e598d5e8f84fd81cba759c1332c66
atishay2305-hub <atishay2305@gmail.com>
2024-04-30 16:21:34 -0400

    cors bug fix

--- Backend/App.mjs
+++ Backend/App.mjs
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000/",
+  origin: "http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
