58ba11c8924530df92a2ecb4d3c51e98591769d8
atishay2305-hub <atishay2305@gmail.com>
2024-04-30 12:58:56 -0400

    removed app listened for vercel

--- Backend/App.mjs
+++ Backend/App.mjs
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "https://leaf-log-4kpa.vercel.app/",
+  origin: "https://leaf-log-4kpa.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
@@ -292,5 +292,4 @@ transporter.verify(function (error, success) {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+export default app;
