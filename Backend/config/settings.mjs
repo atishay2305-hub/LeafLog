ccb66c69fbad7ea3fafc9d93e779d7437420b323
atishay2305-hub <atishay2305@gmail.com>
2024-04-30 12:00:25 -0400

    adding vercel

--- Backend/config/settings.mjs
+++ Backend/config/settings.mjs
@@ -1,4 +1,4 @@
 export const mongoConfig = {
-  serverUrl: 'mongodb+srv://atishay2305:d1N73QqeRIPbIlAS@leaflog.qc1rin7.mongodb.net/?retryWrites=true&w=majority&appName=leaflog',
-  database: 'leaflog'
+  serverUrl: 'mongodb://localhost:27017',
+  database: 'ChatBot'
 };
