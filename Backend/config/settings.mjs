a8d1018fcf2da285e952b932941da116991d0ef3
atishay2305-hub <atishay2305@gmail.com>
2024-05-01 10:11:50 -0400

    .

--- Backend/config/settings.mjs
+++ Backend/config/settings.mjs
@@ -1,9 +1,3 @@
-// export const mongoConfig = {
-//   serverUrl: process.env.MONGO_SERVER_URL || 'mongodb://localhost:27017',
-//   database: process.env.MONGO_DATABASE || 'leaflog'
-// };
-
-  
 export const mongoConfig = {
   serverUrl: process.env.MONGO_SERVER_URL || 'mongodb://localhost:27017',
   database: process.env.MONGO_DATABASE || 'leaflog'
